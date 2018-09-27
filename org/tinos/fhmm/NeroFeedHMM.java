e679800e1944890eb13d0b1b294c2353a8997c98
罗瑶光 <lyg.tin@gmail.com>
2018-09-27 14:44:30 +0800

    Update NeroFeedHMM.java

--- org/tinos/fhmm/NeroFeedHMM.java
+++ org/tinos/fhmm/NeroFeedHMM.java
@@ -4,6 +4,6 @@ import org.tinos.obj.FDHMMNode;
 import org.tinos.zabbi.DataString;
 public interface NeroFeedHMM{
 	@SuppressWarnings(DataString.RAW_TYPES)
-	public String getPrettyRecurWord(String temp, String input, int i, int length, LinkedHashMap<Integer, LinkedHashMap> roots);
+	public String getPrettyRecurWord(String temp, String input, int i, int length, LinkedHashMap<Integer, LinkedHashMap> roots, int depth);
 	public String getFastRecurWord(String temp, LinkedHashMap<String, FDHMMNode> maps, String input, int i, int length);
 }
