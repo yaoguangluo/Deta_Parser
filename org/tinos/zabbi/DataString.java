abc1fd85674ee48b2798e3217b0f7bd4ec0ef19d
罗瑶光 <lyg.tin@gmail.com>
2018-09-27 14:48:52 +0800

    Update DataString.java

--- org/tinos/zabbi/DataString.java
+++ org/tinos/zabbi/DataString.java
@@ -8,9 +8,14 @@ public class DataString{
 	
 	public static final int INT_ZERO = 0;
 	public static final int INT_ONE = 1;
+	public static final int INT_TWO = 2;
+	public static final int INT_THREE = 3;
+	public static final int INT_FOUR = 4;
 	public static final int INT_SIXTEEN = 16;
 	public static final int INT_SIXTY_FOUR = 64;
 	public static final String GBK_STRING = "GBK";
 	public static final String UTF8_STRING = "UTF8";
 	public static final String WORDS_SOURSE_LINK = "words.lyg";
+	public static final String OGLD_SOURSE_LINK = "ogld.lyg";
+	public static final String CY_SOURSE_LINK = "cy.lyg";
 }
