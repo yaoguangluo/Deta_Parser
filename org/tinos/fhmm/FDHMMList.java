eebaace8717633580545623b049ab5fbc851bc81
罗瑶光 <lyg.tin@gmail.com>
2018-09-27 14:44:05 +0800

    Update FDHMMList.java

--- org/tinos/fhmm/FDHMMList.java
+++ org/tinos/fhmm/FDHMMList.java
@@ -8,5 +8,7 @@ public interface FDHMMList{
 	@SuppressWarnings(DataString.RAW_TYPES)
 	public LinkedHashMap<Integer, LinkedHashMap> getRoot() ;
 	public LinkedHashMap<String, FDHMMNode> getMap() ;
+	public String getEuclid() ;
+	public LinkedHashMap<String, Integer> getChengYu();
 	
 }
