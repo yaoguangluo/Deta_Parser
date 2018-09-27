b5373b9ccda98d89d226d1da6271bf21d3a05c3f
罗瑶光 <lyg.tin@gmail.com>
2018-09-27 14:43:27 +0800

    Update PrettyAnalyzerImp.java

--- org/tinos/engine/imp/PrettyAnalyzerImp.java
+++ org/tinos/engine/imp/PrettyAnalyzerImp.java
@@ -22,14 +22,42 @@ public class PrettyAnalyzerImp implements  PrettyAnalyzer{
 	
 	@SuppressWarnings(DataString.RAW_TYPES)
 	public List<String> parserString(String input) {
+		LinkedHashMap <String, Integer> chengYu = fDHMMList.getChengYu();
+		String euclid = fDHMMList.getEuclid();
 		List<String> output = new ArrayList<String>();
 		LinkedHashMap <Integer, LinkedHashMap> roots = fDHMMList.getRoot();
-		int length=input.length();
+		int length = input.length();
+		int depth=0;
 		for(int i = DataString.INT_ZERO; i < length; i += DataString.INT_ZERO){
-			String char_i = DataString.EMPTY_STRING+input.charAt(i);
+			String char_i = DataString.EMPTY_STRING + input.charAt(i);
 			String temp = char_i ;
-			temp = neroFeedHMM.getPrettyRecurWord(temp,input,i,length,roots);
-			output.add(temp);
+			temp = neroFeedHMM.getPrettyRecurWord(temp,input,i,length,roots, depth);
+			if(temp.length() == DataString.INT_THREE) {
+					if(euclid.contains(DataString.EMPTY_STRING + temp.charAt(DataString.INT_ZERO))){
+						String temps[] = temp.split(DataString.EMPTY_STRING + temp.charAt(DataString.INT_ZERO));
+						output.add(DataString.EMPTY_STRING + temp.charAt(DataString.INT_ZERO));
+						output.add(DataString.EMPTY_STRING + temps[DataString.INT_ONE]);
+					}else if(euclid.contains(DataString.EMPTY_STRING +temp.charAt(DataString.INT_TWO))){
+						String temps[] = temp.split(""+temp.charAt(DataString.INT_TWO));
+						output.add(DataString.EMPTY_STRING + temps[DataString.INT_ZERO]);
+						output.add(DataString.EMPTY_STRING + temp.charAt(DataString.INT_TWO));
+					}else {
+						output.add(DataString.EMPTY_STRING + temp);
+					}
+			}else if(temp.length() == DataString.INT_FOUR) {
+				if(chengYu.containsKey(temp)){
+					output.add(temp);
+				}else {
+					output.add(DataString.EMPTY_STRING +temp.charAt(DataString.INT_ZERO)+temp.charAt(DataString.INT_ONE));
+					output.add(DataString.EMPTY_STRING +temp.charAt(DataString.INT_TWO)+temp.charAt(DataString.INT_THREE));
+				}
+			}else if(temp.length() == DataString.INT_TWO){
+				output.add(temp);
+			}else{
+				for(int j = DataString.INT_ZERO; j < temp.length(); j++) {
+					output.add(DataString.EMPTY_STRING + temp.charAt(j));
+				}
+			}			
 			int t_length=temp.length();
 			i += (t_length == DataString.INT_ZERO ? DataString.INT_ONE : t_length);
 		}
