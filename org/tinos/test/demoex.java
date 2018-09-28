2c9a765c7a99928a1e34216ed684a97657548e84
罗瑶光 <lyg.tin@gmail.com>
2018-09-29 00:28:35 +0800

    Update demoex.java

--- org/tinos/test/demoex.java
+++ org/tinos/test/demoex.java
@@ -1,37 +1,38 @@
-﻿package org.tinos.test;
+*package org.tinos.test;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.tinos.engine.Analyzer;
-import org.tinos.engine.imp.PrettyAnalyzerImp;
-
+import org.tinos.engine.imp.BinaryForestAnalyzerImp;
 import timeProcessor.TimeCheck;
 public class demoex{
 	public static void main(String args[]) throws IOException{
-		Analyzer analyzer=new PrettyAnalyzerImp() ;
+		Analyzer analyzer = new BinaryForestAnalyzerImp() ;
 		analyzer.init();
 		//返回分词数据集合
 		List<String> sets = new ArrayList<String>();
-		TimeCheck tc= new TimeCheck();
-		String ss = "明月几时有，把酒问青天，不知天上宫阙，今夕是何年，"
- 		+"    〔治疗〕户，照无眠，不应有恨，和是偏向别时圆，人有悲欢离合，月有阴晴圆缺"
-				+ "此事古难全，但愿人常久，千里共婵娟， 从容易开始";
-		ss=ss.replace("\r\n", "");
+		TimeCheck tc = new TimeCheck();
+		String ss = "从容易开始能从容不迫" + 
+				""; //大概10字
+		//System.out.print("输入：" + ss);
+		System.out.println("");
+		System.out.println("计时开始");
 		tc.begin();
-		for(int i=0;i<400;i++) {
+		for(int i = 0; i < 1000000; i++) {//重复执行 100万次 相当于1000 万字分词
 			sets = analyzer.parserString(ss); 
 		}
 		tc.end();;
+		System.out.println("计时结束");
+		tc.duration();
+		System.out.println("分词效果如下");
 		int j=0;
 		for(int i = 0; i < sets.size(); i++){
-			System.out.print(sets.get(i)+"-");
+			System.out.print(sets.get(i)+"  |  ");
 			j++;
-			if(j>25) {
+			if(j>15) {//每行15个词语输出
 				j=0;
 				System.out.println("");
 			}
 		}
-		tc.duration();
 	}
 }
