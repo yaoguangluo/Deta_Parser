3c1fb04cb373c3d06932228cb1f660db8387a8bf
罗瑶光 <lyg.tin@gmail.com>
2018-10-01 12:32:23 +0800

    Update Utils.java

--- org/tinos/utils/Utils.java
+++ org/tinos/utils/Utils.java
@@ -4,11 +4,11 @@ import org.tinos.obj.FLHMMNode;
 import org.tinos.obj.FHHMMNode;
 import org.tinos.zabbi.DataString;
 public abstract interface Utils{
-	@SuppressWarnings({ DataString.RAW_TYPES})
+	@SuppressWarnings({DataString.RAW_TYPES})
 	public Map <Integer, Map> linerEuclid(Map<String, FLHMMNode> linkedHashMap);
-	@SuppressWarnings({ DataString.RAW_TYPES})
+	@SuppressWarnings({DataString.RAW_TYPES})
 	public Map<Integer, Map> hashEuclid(Map<String, FHHMMNode> linkedHashMap);	
-	@SuppressWarnings({ DataString.RAW_TYPES})
+	@SuppressWarnings({DataString.RAW_TYPES})
 	public Map<Integer, Map> mcogsEuclid(Map<String, FHHMMNode> concurrentHashMap);
 }
 
