f7dbe54bc778b18ac084ebcff03a3159174d63d4
yaoguangluo <fengyue1985>
2018-10-13 08:26:53 +0800

    一直在优化5.0.7

--- main/src/org/tinos/engine/nlp/imp/NLPControllerImp.java
+++ main/src/org/tinos/engine/nlp/imp/NLPControllerImp.java
@@ -16,46 +16,51 @@ public class NLPControllerImp implements NLPController {
             return countInputStringLength;
         } else {
             countInputStringLength = doPOSAndEMMCheck(countInputStringLength
-                    - StableData.INT_ONE, output, wordsForest, StableData.EMPTY_STRING
-                    + inputString.charAt(StableData.INT_ZERO) + inputString.charAt(StableData.INT_ONE)
-                    + inputString.charAt(StableData.INT_TWO), prefixWord, posUtils);
+                    - StableData.INT_ONE, output, wordsForest, StableData.EMPTY_STRING + inputString
+                    .charAt(StableData.INT_ZERO) + inputString.charAt(StableData.INT_ONE) + inputString
+                    .charAt(StableData.INT_TWO), prefixWord, posUtils);
             return countInputStringLength;
         }
     }
 
     public int doPOSAndEMMCheck(int countInputStringLength, List<String> outputList, Map<String, String> wordsForest
             , String inputString, String[] prefixWord, POSController posUtils) {
-        if (wordsForest.containsKey(inputString)) {
-            outputList.add(inputString);
+        if (prefixWord[StableData.INT_ZERO] == null) {
             prefixWord[StableData.INT_ZERO] = inputString;
             return countInputStringLength;
         }
         String[] strings = new String[StableData.INT_FOUR];
         strings[StableData.INT_ZERO] = StableData.EMPTY_STRING + inputString.charAt(StableData.INT_ZERO);
-        strings[StableData.INT_ONE] = StableData.EMPTY_STRING + inputString.charAt(StableData.INT_ZERO)
-                + inputString.charAt(StableData.INT_ONE);
-        strings[StableData.INT_TWO] = StableData.EMPTY_STRING + inputString.charAt(StableData.INT_ONE)
-                + inputString.charAt(StableData.INT_TWO);
+        strings[StableData.INT_ONE] = StableData.EMPTY_STRING + inputString.charAt(StableData.INT_ZERO) + inputString
+                .charAt(StableData.INT_ONE);
+        strings[StableData.INT_TWO] = StableData.EMPTY_STRING + inputString.charAt(StableData.INT_ONE) + inputString
+                .charAt(StableData.INT_TWO);
         strings[StableData.INT_THREE] = StableData.EMPTY_STRING + inputString.charAt(StableData.INT_TWO);
-        if (wordsForest.get(strings[StableData.INT_ZERO]).contains(StableData.NLP_LIANG_CI)) {
-            countInputStringLength = posUtils.chuLiLiangCi(wordsForest, outputList, countInputStringLength
-                    , strings, prefixWord);
+        if (!wordsForest.containsKey(strings[StableData.INT_ZERO])) {
+            countInputStringLength = doSlangPartCheck(countInputStringLength - StableData
+                    .INT_ONE, outputList, strings[StableData.INT_ONE], wordsForest, prefixWord);
             return countInputStringLength;
-        } else if (wordsForest.get(strings[StableData.INT_ZERO]).contains(StableData.NLP_JIE_CI)) {
+        }
+        if (wordsForest.get(strings[StableData.INT_ZERO]).contains(StableData.NLP_LIANG_CI)) {
+            countInputStringLength = posUtils.chuLiLiangCi(wordsForest, outputList, countInputStringLength, strings
+                    , prefixWord);
+            return countInputStringLength;
+        }
+        if (wordsForest.get(strings[StableData.INT_ZERO]).contains(StableData.NLP_JIE_CI)) {
             countInputStringLength = posUtils.chuLiJieCi(wordsForest, outputList, countInputStringLength, strings
                     , prefixWord);
             return countInputStringLength;
-        } else if (wordsForest.get(strings[StableData.INT_ZERO]).contains(StableData.NLP_LIAN_CI)) {
+        }
+        if (wordsForest.get(strings[StableData.INT_ZERO]).contains(StableData.NLP_LIAN_CI)) {
             countInputStringLength = posUtils.chuLiLianCi(wordsForest, outputList, countInputStringLength, strings
                     , prefixWord);
             return countInputStringLength;
-        } else if (wordsForest.get(strings[StableData.INT_ZERO]).contains(StableData.NLP_ZHU_CI)) {
+        }
+        if (wordsForest.get(strings[StableData.INT_ZERO]).contains(StableData.NLP_ZHU_CI)) {
             countInputStringLength = posUtils.chuLiZhuCi(wordsForest, outputList, countInputStringLength, strings
                     , prefixWord);
             return countInputStringLength;
         }
-        countInputStringLength = doSlangPartCheck(countInputStringLength - StableData.INT_ONE
-                , outputList, strings[StableData.INT_ONE], wordsForest, prefixWord);
         return countInputStringLength;
     }
 
