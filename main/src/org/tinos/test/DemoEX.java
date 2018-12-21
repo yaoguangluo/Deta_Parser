9ecec07944801741e60d077f3a7ca467bf5dc38e
yaoguangluo <fengyue1985>
2018-12-21 12:33:08 +0800

    1。最新版本支持部分中文短句翻译 2。强化分词特效 3。语义中英混合分词 4。复杂符号处理 5。分词对象化。请仔细调试TEST文件里的DEMO程序函数。 6。 英语词库2万8千单词。 谢谢，祝我女儿圣诞快乐！

--- main/src/org/tinos/test/DemoEX.java
+++ main/src/org/tinos/test/DemoEX.java
@@ -1,63 +1,78 @@
 package org.tinos.test;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.tinos.engine.analysis.Analyzer;
 import org.tinos.engine.analysis.imp.CogsBinaryForestAnalyzerImp;
 import org.tinos.view.obj.WordFrequency;
 import timeProcessor.TimeCheck;
-
-@SuppressWarnings("unused")
 public class DemoEX {
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    public static void main(String[] args) throws IOException {
-        Analyzer analyzer = new CogsBinaryForestAnalyzerImp();
-        analyzer.init();
-        Map<String, String> pos = analyzer.getWord();
-        List<String> sets = new ArrayList<>();
-        TimeCheck t = new TimeCheck();
-        // String ss = "和尚未出家前这个和尚未和尚未成家之人组成家庭过，各位和尚们不要怪这个和尚";
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws IOException {
+		Analyzer analyzer = new CogsBinaryForestAnalyzerImp();
+		analyzer.init();
+		Map<String, String> pos = analyzer.getPosCnToCn();
+		List<String> sets = new ArrayList<>();
+		Map<String, WordFrequency> seta = new ConcurrentHashMap<>();
+		TimeCheck t = new TimeCheck();
+		String ss = "科学的发展是一种传承，每一个获得诺贝尔奖的科学家，都是通过长时间对问题的优化中不断总结和分化" +
+				"，最终得到科学的成果痴呆鉴别";//65字
+//		String ss = "科学，是方法论和逻辑观的具体称谓，是一hgjj345345种劳动传承，和尚未解开谜团的盒子一样，需要打"
+//				  + "开。我们r不需fdfgsdsf要猜测，只要234324保持一颗严谨的心gfdgbdfgfd态。可以用榔头，也"
+//				  + "可以用钥匙，甚至能躺着让它自rwerwer动打开，这就是方法论。持32423续的在某一个ewrwer问题上"
+//				  + "，rewr进行反复的优化，细化，挖rewrwe掘，提纯，分化根据不同rewrewr的思考方式发展不同5435"
+//				  + "345的研究路rwer线，究根掘底，千锤百炼，最后得到的东西，那就是rewrwer真理这就是逻辑观结婚的"
+//				  + "和尚未结婚的等7dnvy之人都和尚未成佛的和尚未必一样和尚未来的和尚udbfj开始念经那和尚未进行"
+//				  + "告别不显得从容易知和"
+//				  + "尚nic8d主一样其"
+//				  + "实都不和尚未成佛的心"
+//				  + "态hdu72和尚未成佛";//341字
+		t.begin();
+		for (int i = 0; i < 1000000; i++) { //重复100万次数 相当于处理 6500多万字
+			//sets = analyzer.parserString(ss);//词性分析
+			seta = analyzer.parserStringByReturnFrequencyMap(ss);
+			//new Quick6DLYGWithStringSwap();
+		}
 
-        String ss = "科学的发展是一种传承，每一个获得诺贝尔奖的科学家，都是通过长时间对问题的优化中不断总结和分化" +
-                "，最终得到科学的成果";
-        t.begin();
-        for (int i = 0; i < 1000000; i++) { //重复100万次数 相当于处理 5700来万字
-            sets = analyzer.parserString(ss);//词性分析
-        }
-        t.end();
-        System.out.print("分析处理真实结果-->");
-        for (int i = 0; i < sets.size(); i++) {
-            if (!sets.get(i).equals("")) {
-                System.out.print(sets.get(i) + " ");
-            }
-        }
-        System.out.println("");
-        t.duration();
-        System.out.println("");
-        System.out.println("词性分析-->");
-        t.begin();
-        for (int j = 0; j < 1; j++) {
-            for (int i = 0; i < sets.size(); i++) {
-                if (!sets.get(i).replaceAll("\\s+", "").equals("")) {
-                    System.out.print(sets.get(i) + "/" + pos.get(sets.get(i)) + "----");
-                }
-            }
-        }
-        t.end();
-        System.out.println("");
-        t.duration();
-        System.out.println("");
-        System.out.println("词频分析-->");
-        t.begin();
-        List<WordFrequency> fwa = analyzer.getWordFrequency(sets);
-        t.end();
-        for (int i = fwa.size() - 1; i >= 0; i--) {
-            System.out.print(fwa.get(i).getWord() + ":" + fwa.get(i).getFrequency() + "----");
-        }
-        System.out.println("");
-        t.duration();
-    }
+		t.end();
+		System.out.print("分析处理真实结果-->");
+//		for (int i = 0; i < seta.size(); i++) {
+//			if (!seta.get(i).equals("")) {
+//				System.out.print(seta.get(i) + " ");
+//			}
+//		}
+//		for (int i = 0; i < sets.size(); i++) {
+//			if (!sets.get(i).equals("")) {
+//				System.out.print(sets.get(i) + " ");
+//			}
+//		}
+		System.out.println("");
+		t.duration();
+		System.out.println("");
+		System.out.println("词性分析-->");
+		t.begin();
+		for (int j = 0; j < 1; j++) {
+			for (int i = 0; i < sets.size(); i++) {
+				if (!sets.get(i).replaceAll("\\s+", "").equals("")) {
+					System.out.print(sets.get(i) + "/" + pos.get(sets.get(i)) + "----");
+				}
+			}
+		}
+		t.end();
+		System.out.println("");
+		t.duration();
+		System.out.println("");
+		System.out.println("词频分析-->");
+		t.begin();
+		Map<Integer, WordFrequency> fwa = analyzer.getWordFrequencyByReturnSortMap(sets);
+		t.end();
+		for (int i = fwa.size() - 1; i >= 0; i--) {
+			System.out.print(fwa.get(i).getWord() + ":" + fwa.get(i).getFrequency() + "----");
+		}
+		System.out.println("");
+		t.duration();
+	}
 }
