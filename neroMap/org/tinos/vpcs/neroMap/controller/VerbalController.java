5a0b749d0bba7022fdb6c1c6b47cbe6867804172
yaoguangluo <fengyue1985>
2019-03-03 08:46:31 +0800

    第二主题研发已经开始

---
+++ neroMap/org/tinos/vpcs/neroMap/controller/VerbalController.java
@@ -0,0 +1,21 @@
+package org.tinos.vpcs.neroMap.controller;
+
+public class VerbalController{
+	//近义词synonyms
+	
+	//反义词antonyms
+	
+	//词性the part of speech
+	
+	//词根root
+	
+	//词频word frequency
+	
+	//词态word fix
+	
+	//词感sense of word
+	
+	//词音pronounciation
+	
+	//翻译translation
+}
