836c143cf5a67ce7ee52cd720a96b350dfde2701
yaoguangluo <fengyue1985>
2019-03-21 13:57:31 +0800

    ICA training swap to CNN measure kernel

--- behaviorMap/org/tinos/behavior/test/EducationLevelTest.java
+++ behaviorMap/org/tinos/behavior/test/EducationLevelTest.java
@@ -1,6 +1,6 @@
 package org.tinos.behavior.test;
 public class EducationLevelTest{
-	public double getEducation(double[] meansureICA) {
+	public double getEducation(double[] measureICA) {
 		//deta r with CNN ICA per epp algorithm
 		
  		return 0;
