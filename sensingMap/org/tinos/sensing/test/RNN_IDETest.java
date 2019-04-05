40469b6813a8dd692832078173e449d8bbac5418
yaoguangluo <fengyue1985>
2019-04-05 16:53:38 +0800

    RNN 内核优化 1 .

--- sensingMap/org/tinos/sensing/test/RNN_IDETest.java
+++ sensingMap/org/tinos/sensing/test/RNN_IDETest.java
@@ -50,8 +50,10 @@ public class RNN_IDETest{
 			for(int i = 0; i < list.size(); i++) {
 				for(int j = i + 1; j < list.size(); j++) {
 					dovCount ++;
-					dovFactor += Math.abs(list.get(i) - list.get(j));
+					//dovFactor += Math.abs(list.get(i) - list.get(j));
+					dovFactor += list.get(j);
 				}
+				dovFactor += Math.abs(list.get(i) - dovFactor);
 				eopFactor += (eopFactor + list.get(i)) / 2;
 			}
 			//pos normalization
@@ -111,8 +113,10 @@ public class RNN_IDETest{
 			for(int i = 0; i < list.size(); i++) {
 				for(int j = i + 1; j < list.size(); j++) {
 					dovCount ++;
-					dovFactor += Math.abs(list.get(i) - list.get(j));
+//					dovFactor += Math.abs(list.get(i) - list.get(j));
+					dovFactor += list.get(j);
 				}
+				dovFactor += Math.abs(list.get(i) - dovFactor);
 				eopFactor += (eopFactor + list.get(i)) / 2;
 			}
 			//pos normalization
