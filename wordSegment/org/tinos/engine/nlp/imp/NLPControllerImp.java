669c5107c810dc244edd83ca32cc79575f82e3ba
yaoguangluo <fengyue1985>
2019-05-03 15:14:46 +0800

    新增的5种新卷积核进行修正.

--- wordSegment/org/tinos/engine/nlp/imp/NLPControllerImp.java
+++ wordSegment/org/tinos/engine/nlp/imp/NLPControllerImp.java
@@ -102,9 +102,11 @@ public class NLPControllerImp implements NLPController{
 		}
 		if (StableMaps.dongCi.containsKey(strings[StableData.INT_ZERO])||StableMaps.fuCi.containsKey(strings[StableData.INT_ZERO])){
 			if(StableMaps.zhuCi.containsKey(prefixWord[StableData.INT_ZERO].toString())
-					&& (StableMaps.mingCi.containsKey(strings[StableData.INT_TWO])
-					|| StableMaps.fuCi.containsKey(strings[StableData.INT_TWO])
-					|| StableMaps.liangCi.containsKey(strings[StableData.INT_TWO]))) {
+					&& StableMaps.liangCi.containsKey(strings[StableData.INT_TWO])) {
+				countInputLength = posUtils.parserFirstCharOfThree(countInputLength, outputList, strings, prefixWord);
+				return countInputLength;
+			}
+			if(StableMaps.fuCi.containsKey(strings[StableData.INT_TWO])||StableMaps.mingCi.containsKey(strings[StableData.INT_TWO])) {
 				countInputLength = posUtils.parserFirstCharOfThree(countInputLength, outputList, strings, prefixWord);
 				return countInputLength;
 			}
