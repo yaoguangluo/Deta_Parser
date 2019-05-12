f0484e2ed287d2d1a65cb2d9eb3c70e6b40b148a
yaoguangluo <fengyue1985>
2019-05-13 00:42:47 +0800

    增加4字非成语的量词处理.

--- wordSegment/org/tinos/engine/nlp/imp/NLPControllerImp.java
+++ wordSegment/org/tinos/engine/nlp/imp/NLPControllerImp.java
@@ -147,7 +147,7 @@ public class NLPControllerImp implements NLPController{
 			return countInputStringLength;
 		}
 		if(StableMaps.mingCi.containsKey(preRegister)|| StableMaps.xingRongCi.containsKey(preRegister)|| StableMaps.fuCi.containsKey(preRegister)
-				|| StableMaps.dongCi.containsKey(preRegister)|| StableMaps.lianCi.containsKey(preRegister)) {
+				|| StableMaps.dongCi.containsKey(preRegister)|| StableMaps.lianCi.containsKey(preRegister)|| StableMaps.liangCi.containsKey(preRegister)) {
 			if(StableMaps.mingCi.containsKey(postRegister)|| StableMaps.dongCi.containsKey(postRegister)
 					|| StableMaps.xingRongCi.containsKey(postRegister)|| StableMaps.xingWeiCi.containsKey(postRegister)
 					|| StableMaps.fuCi.containsKey(postRegister)|| StableMaps.jieCi.containsKey(postRegister)) {
