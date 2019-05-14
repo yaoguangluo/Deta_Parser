2e5ee18abd2e15c8f24bf0168bd5e824b9eec462
yaoguangluo <fengyue1985>
2019-05-15 07:09:49 +0800

    增加介词前缀四字卷积处理 2019-05-15

--- wordSegment/org/tinos/engine/nlp/imp/NLPControllerImp.java
+++ wordSegment/org/tinos/engine/nlp/imp/NLPControllerImp.java
@@ -156,8 +156,9 @@ public class NLPControllerImp implements NLPController{
 					, stringBuilder.delete(StableData.INT_THREE, StableData.INT_FOUR), prefixWord, posUtils, charPosition, textInputString);
 			return countInputStringLength;
 		}		
-		if(StableMaps.mingCi.containsKey(preRegister)|| StableMaps.xingRongCi.containsKey(preRegister)|| StableMaps.fuCi.containsKey(preRegister)
-				|| StableMaps.dongCi.containsKey(preRegister)|| StableMaps.lianCi.containsKey(preRegister)|| StableMaps.liangCi.containsKey(preRegister)) {
+		if(StableMaps.jieCi.containsKey(preRegister)|| StableMaps.mingCi.containsKey(preRegister)|| StableMaps.xingRongCi.containsKey(preRegister)
+				|| StableMaps.fuCi.containsKey(preRegister)|| StableMaps.dongCi.containsKey(preRegister)|| StableMaps.lianCi.containsKey(preRegister)
+				|| StableMaps.liangCi.containsKey(preRegister)) {
 			if(StableMaps.mingCi.containsKey(postRegister)|| StableMaps.dongCi.containsKey(postRegister)
 					|| StableMaps.xingRongCi.containsKey(postRegister)|| StableMaps.xingWeiCi.containsKey(postRegister)
 					|| StableMaps.fuCi.containsKey(postRegister)|| StableMaps.jieCi.containsKey(postRegister)) {
